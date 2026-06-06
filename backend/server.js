5921882cd43b56610de8e79996ca506c2bb72a39
Khushimehta-hub <khushim1626@gmail.com>
2026-06-06 13:19:15 +0530

    fix server cors

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,19 @@ const feedbackRoutes = require("./routes/feedbackRoutes");
 
 const app = express();
 
-app.use(cors());
+// Middleware
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 
-app.get("/", (req,res)=>{
+// Test route
+app.get("/", (req, res) => {
     res.send("Mindlabz Internship Management Portal Running");
 });
-
 
 // API routes
 app.use("/api/users", userRoutes);
@@ -24,16 +30,19 @@ app.use("/api/attendance", attendanceRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
 
-// paste your mongodb string here
+// MongoDB connection
 mongoose.connect("mongodb+srv://khushim1626_db_user:VijayRenu@cluster0.tlzazh5.mongodb.net/?appName=Cluster0")
-.then(()=>{
+.then(() => {
     console.log("MongoDB Connected Successfully");
 })
-.catch((err)=>{
+.catch((err) => {
     console.log(err);
 });
 
 
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000");
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
